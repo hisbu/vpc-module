44c6fc55030615d26698357c657ff355ed6b8331
hisbu <43975159+hisbu@users.noreply.github.com>
2020-07-14 06:27:29 +0700

    first"

---
+++ interface.tf
@@ -0,0 +1,19 @@
+variable "name"{
+    description = "the name"
+}
+
+variable "cidr"{
+
+}
+
+variable "public_subnet"{
+
+}
+
+variable "enable_dns_hostnames" {
+    default = true
+}
+
+variable "enable_dns_support"{
+    default = true
+}
